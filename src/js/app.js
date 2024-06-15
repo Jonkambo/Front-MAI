631c7aa5805de3268494057bbf75eb53e8a98460
Jonkambo <dairrow1@mail.ru>
2024-06-15 20:14:58 +0300

    create dialog (not working)

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import dropdownListAnimation from "./modules/dropdown-anim"
+import requestDialogue from "./modules/question-dialogue"
 // import selectInfo from "./modules/select-info"
 
 const init = () => {
     dropdownListAnimation()
+    requestDialogue()
     // selectInfo()
 }
 
