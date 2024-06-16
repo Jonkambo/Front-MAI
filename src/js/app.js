6d5a5c35cafe1d5bccd63585ed78b18192aa6bfb
Jonkambo <dairrow1@mail.ru>
2024-06-16 22:43:36 +0300

    full create menu git add .git add .git add .git add .git add .!

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 import dropdownListAnimation from "./modules/dropdown-anim"
 import "./modules/question-dialogue"
 import "./modules/select-info"
+import "./modules/main-menu"
 
 const init = () => {
     dropdownListAnimation();
+    // mainMenuAppear();
     // requestDialogue();
     // animatedPinnedMenu()
 }
 
 init()
+
