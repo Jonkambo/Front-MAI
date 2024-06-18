4eb7fbe58f63aa94c333cf3afb6b512e5655ed24
Jonkambo <dairrow1@mail.ru>
2024-06-19 01:09:47 +0300

    create visually impaired menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import dropdownListAnimation from "./modules/dropdown-anim"
 import "./modules/question-dialogue"
 import "./modules/select-info"
 import "./modules/main-menu"
+import "./modules/visually-impared-menu-appear"
 
 const init = () => {
     dropdownListAnimation();
